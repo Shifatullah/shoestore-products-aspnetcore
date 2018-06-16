7968d81db668c7134d7ccd7b2efb808d2bef79b9
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-06-17 08:50:39 +1000

    Created RESTful API in ASP.Net Core

---
+++ ShoeStore.Products.AspNetCore/Controllers/ProductsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ShoeStore.Products.Domain;
+
+namespace ShoeStore.Products.AspNetCore.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ProductsController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
+
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "iPhone 8",
+                Code = "iP8",
+                Cost = 1000,
+                ShortDescription = "iPhone 8 mobile phone",
+                LongDescription = "iPhone 8 mobile phone is better",
+                Rank = 1,
+                Stock = 3
+            });
+
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "iPhone X",
+                Code = "iPX",
+                Cost = 2000,
+                ShortDescription = "iPhone X mobile phone",
+                LongDescription = "iPhone X mobile phone is better",
+                Rank = 2,
+                Stock = 5
+            });
+
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "iPhone 7",
+                Code = "iP7",
+                Cost = 2000,
+                ShortDescription = "iPhone 7 mobile phone",
+                LongDescription = "iPhone 7 mobile phone is better",
+                Rank = 3,
+                Stock = 8
+            });
+
+            return products;
+        }
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ ShoeStore.Products.Domain/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShoeStore.Products.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int Rank { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string LongDescription { get; set; }
+
+        public decimal Cost { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
